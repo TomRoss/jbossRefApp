d70b1da8de453a34538471f785b3cdad06adf597
Tom Ross <tom.ross@redhat.com>
2014-03-13 16:53:34 +0000

    Added an option to run on  OS X.

--- apps/minibankref1Test/src/main/java/org/app/minibank/minibankref1/mdb/JMSTestUtil.java
+++ apps/minibankref1Test/src/main/java/org/app/minibank/minibankref1/mdb/JMSTestUtil.java
@@ -126,7 +126,15 @@ public final class JMSTestUtil {
         for (JBNode node : nodesToExecCmd) {
             String nodeScript = node.getPath() + TestUtil.PATH_SEPARATOR + commandToExec;
             File fileNode = new File(nodeScript);
-            String line = "cmd /C " + fileNode.getName();
+            
+            String line = null;
+            if (TestUtil.isWindows()){
+            	line = "cmd /C " + fileNode.getName();
+            }
+            if (TestUtil.isMac()){
+            	line = "/bin/bash " + fileNode.getName();
+            }
+            
             log.info(getMesageInfo() + " start launching '" + line + "' for node:" + node);
             CommandLine cmdLine = CommandLine.parse(line);
             DefaultExecuteResultHandler resultHandler = new DefaultExecuteResultHandler();
